b6cbcefd59382dfff65db2f96c56b98d04474fdd
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-14 20:48:28 +0200

    ADD pais input component and pais table component, FIX por pais component

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,12 +8,23 @@ import { PaisService } from '../../services/pais.service';
   styles: [],
 })
 export class PorPaisComponent {
-  termino: string = 'Hola Mundo';
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
   constructor(private paisService: PaisService) {}
-  buscar() {
-    console.log(this.termino);
-    this.paisService.buscarPais(this.termino).subscribe((resp) => {
-      console.log(resp);
-    });
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
 }
